1dc53b1c3b7c05a92bf0589ca4dd1a2c5c47cc35
Mamona Abdullah <mamonaabdullah122@gmail.com>
2022-01-29 14:44:10 +0500

    rxjs part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -16,32 +16,33 @@ export class ContactComponent implements OnInit {
   feedbackFormDirective!:any;
 
   formErrors = {
-    firstname: '',
-    lastname: '',
-    phone: '',
-    email: ''
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
   };
 
   validationMessages = {
-    firstname: {
-      required: 'First name is required.',
-      minlength: 'First name must be at least 2 characters long.',
-      maxlength: 'Max nb of chars are 25.'
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
     },
-    lastname: {
-      required: 'Last name is required.',
-      minlength: 'Last name must be at least 2 characters long.',
-      maxlength: 'Max nb of chars are 25.'
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
     },
-    phone: {
-      required: 'Phone number is required.',
-      pattern: 'Phone number must contain only numbers.'
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
     },
-    email: {
-      required: 'Email is required.',
-      email: 'Email not in valid format.'
-    }
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
   };
+  fb: any;
 
   constructor(
     private formBuilder: FormBuilder
@@ -54,32 +55,16 @@ export class ContactComponent implements OnInit {
   }
 
   createForm() {
-    this.feedbackForm = this.formBuilder.group({
-      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      phone: ['', [Validators.required, Validators.pattern]],
-      email: ['', [Validators.required, Validators.email]],
+    this.feedbackForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
-
-    this.feedbackForm.valueChanges
-        .subscribe(data => this.onValueChanged(data));
-
-    this.onValueChanged(); // (re)set form validation messages
   }
-
-  onValueChanged(data?: any) {
-    if (!this.feedbackForm) {
-      return;
-    }
-
-    const form = this.feedbackForm;
-
-
-            }
-
 
   onSubmit() {
     this.feedback = this.feedbackForm.value;
@@ -96,3 +81,4 @@ export class ContactComponent implements OnInit {
     this.feedbackFormDirective.resetForm();
   }
 }
+
